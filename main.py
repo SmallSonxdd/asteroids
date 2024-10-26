8f5af878751a64c60b1b53aa09a6622801d040a6
smallsonxdd <jan.lhotak123@gmail.com>
2024-10-26 17:03:38 +0200

    added asteroid and asteroidfield python files that create and control asteroid behavior, updated main file to contain asteroid group, asteroid and asteroidfield containers, added asteroid field object; didnt touch while loop, very clean, very nice

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 import pygame
 from constants import *
 from player import Player
+from asteroidfield import *
 
 def main():
     pygame.get_init()
     clock = pygame.time.Clock()
     dt = 0
+
     updatable_group = pygame.sprite.Group()
     drawable_group = pygame.sprite.Group()
+    asteroids_group = pygame.sprite.Group()
+
     Player.containers = (updatable_group, drawable_group)
+    Asteroid.containers = (asteroids_group, updatable_group, drawable_group)
+    AsteroidField.containers = (updatable_group)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill(000000)
+
         for obj in updatable_group:
             obj.update(dt)
+
         for obj in drawable_group:
             obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60)/1000
+
     print('Starting asteroids!')
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
