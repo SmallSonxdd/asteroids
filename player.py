6bcec99747657262955dff4a7bb8634c6254dc06
smallsonxdd <jan.lhotak123@gmail.com>
2024-10-26 18:05:00 +0200

    player shoots with 0.3 delay now, updated shoot method of PLAYER, updated update method of PLAYER, added shooting cooldown constant

--- player.py
+++ player.py
@@ -11,6 +11,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -27,6 +28,7 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
+        self.timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -58,6 +60,9 @@ class Player(CircleShape):
         self.position += (x_element, y_element)
 
     def shoot(self):
+        if self.timer > 0:
+            return
         shot = Shot(self.position[0], self.position[1])
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         shot.velocity = forward * PLAYER_SHOOT_SPEED
+        self.timer = PLAYER_SHOOT_COOLDOWN
